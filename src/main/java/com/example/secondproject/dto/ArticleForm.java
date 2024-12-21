9e0016b4e72b35a01788684db2441cc7d4eef6c7
goodispotato <potatoisgood@ewhain.net>
2024-12-21 23:47:32 +0900

    13주차

---
+++ src/main/java/com/example/secondproject/dto/ArticleForm.java
@@ -0,0 +1,20 @@
+package com.example.secondproject.dto;
+
+import com.example.secondproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class ArticleForm {
+    private String title;
+
+
+
+    private String content;
+
+
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
+}
